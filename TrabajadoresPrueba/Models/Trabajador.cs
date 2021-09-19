5dab288e202af2a0308da1fcee92c26e8516d97f
Edenmaru <eden_maru1@outlook.com>
2021-09-18 19:51:17 -0500

    clase trabajador para la lista

---
+++ TrabajadoresPrueba/Models/Trabajador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabajadoresPrueba.Models
+{
+    public class Trabajador
+    {
+        public int Id { get; set; }
+        public string TipoDocumento { get; set; }
+        public int NroDocumento { get; set; }
+        public string Nombres { get; set; }
+        public string Sexo { get; set; }
+        public string nombreDepartamento { get; set; }
+        public string nombreProvincia { get; set; }
+        public string nombreDistrito { get; set; }
+    }
+}
